b4ee75cab2de5894382eb18cfe42acf054347be8
Chriiiss97 <ci.carlsson97@outlook.com>
2025-11-04 23:59:37 +0100

    code
    all code

---
+++ index.js
@@ -0,0 +1,46 @@
+Hpoäng = document.getElementById("poäng");
+poäng = 0;
+
+
+
+function enplus() {
+    poäng += 1;
+    Hpoäng.innerText = poäng
+}
+
+function tvåplus() {
+    poäng += 2;
+    Hpoäng.innerText = poäng
+}
+
+function treplus() {
+    poäng += 3;
+    Hpoäng.innerText = poäng
+}
+
+function hreset() {
+    poäng = 0;
+    Hpoäng.innerText = poäng
+}
+
+Bpoäng = document.getElementById("bpoäng");
+bpoäng = 0;
+
+function benplus() {
+    bpoäng += 1;
+    Bpoäng.innerText = bpoäng
+}
+
+function btvåplus() {
+    bpoäng += 2;
+    Bpoäng.innerText = bpoäng
+}
+
+function btreplus() {
+    bpoäng += 3;
+    Bpoäng.innerText = bpoäng
+}
+function breset() {
+    bpoäng = 0;
+    Bpoäng.innerText = bpoäng
+}
